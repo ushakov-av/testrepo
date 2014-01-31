9a0f47d0d0c958fc30b0775a1f18b456993f90f9
Alexander Ushakov <ushakov.av@gmail.com>
2014-01-31 04:27:11 -0800

    Update App.java

--- src/main/java/ru/mipt/test/App.java
+++ src/main/java/ru/mipt/test/App.java
@@ -4,8 +4,13 @@ package ru.mipt.test;
  * Hello world!
  */
 public class App {
+    
+    /**
+     * main.
+     */
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.pringln("wow");
     }
 }
