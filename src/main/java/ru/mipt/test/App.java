54aa8eb8477ddcccd7b0071d3ef2ea365f60d632
Lunix <ushakov.av@gmail.com>
2014-01-31 16:13:42 +0400

    App change commit

--- src/main/java/ru/mipt/test/App.java
+++ src/main/java/ru/mipt/test/App.java
@@ -2,12 +2,10 @@ package ru.mipt.test;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
     }
 }
