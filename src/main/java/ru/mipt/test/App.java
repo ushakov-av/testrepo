072c71e7e1a20426a711bbcef18d4959ba9447f3
Lunix <ushakov.av@gmail.com>
2014-01-31 16:23:57 +0400

    App formatting change.

--- src/main/java/ru/mipt/test/App.java
+++ src/main/java/ru/mipt/test/App.java
@@ -4,6 +4,12 @@ package ru.mipt.test;
  * Hello world!
  */
 public class App {
+
+    /**
+     * Start point.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
